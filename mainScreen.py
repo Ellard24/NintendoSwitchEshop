8bc5a7d960a01c4ef8066d36244975adf9ae2b33
Ellard Gerritsen <egvdh@comcast.net>
2017-06-09 21:12:53 -0400

    we have a selection screen and some text

--- mainScreen.py
+++ mainScreen.py
@@ -1,5 +1,5 @@
 import kivy
-from kivy.uix.gridlayout import GridLayout
+from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.label import Label
 from kivy.uix.button import Button
@@ -8,15 +8,16 @@ from kivy.uix.button import Button
 class SM(ScreenManager):
     def __init__(self, **kwargs):
         super(SM,self).__init__(**kwargs)
-        sc = mainScreen.SelectionScreen()
-        amScene = american.AmericanScene()
+        sc = SelectionScreen()
+        amScene = AmericanScreen()
         self.add_widget(sc)
+        self.add_widget(amScene)
 
 class SelectionScreen(Screen):
 
     def __init__(self, **kwargs):
         super(SelectionScreen, self).__init__(**kwargs)
-        layout = Grid()
+        layout = FloatL()
         self.add_widget(layout)
 
 
@@ -26,22 +27,44 @@ class AmericanScreen(Screen):
         super(AmericanScreen, self).__init__(**kwargs)
         
 
-class Grid(GridLayout):
+class FloatL(FloatLayout):
 
     def __init__(self, **kwargs):
-        super(Grid, self).__init__(**kwargs)
+        super(FloatL, self).__init__(**kwargs)
         self.cols = 1
 
+        #Page name
+        nameLabel = Label(text="Region Selection",
+                        color=[1,1,1,1],
+                        pos_hint = {'x': 0, 'y':0.4})
+        print(nameLabel)
+        self.add_widget(nameLabel)
+
+        #Version
+        versionLabel = Label(text="Version 1.0.0 Ellard Gerritsen",
+                            color=[1,1,1,1],
+                            pos_hint={'x': 0, 'y': -0.45})
+        self.add_widget(versionLabel)
+
         #Button configuration
-        b1 = Button(text="American Games", pos=(self.x, self.height / 2))
+        b1 = Button(text="American Games", 
+            pos_hint={'x': 0.40, 'y': 0.5},
+            size_hint_x = 0.2,
+            size_hint_y = 0.2)
         b1.bind(on_press=self.buttonTest)
         self.add_widget(b1)
 
-        b2 = Button(text="European Games", pos=(self.x, self.height / 2))
+        b2 = Button(text="European Games",
+             pos_hint={'x': 0.40, 'y': 0.3},
+             size_hint_x = 0.2,
+             size_hint_y = 0.2)
         b2.bind(on_press=self.buttonTest)
         self.add_widget(b2)
 
-        b3 = Button(text="Japanese Games", pos=(self.x, self.height / 2))
+        b3 = Button(text="Japanese Games",
+             pos_hint={'x': 0.40, 'y': 0.1},
+             size_hint_x = 0.2,
+             size_hint_y = 0.2)
         b3.bind(on_press=self.buttonTest)
         self.add_widget(b3)
 
